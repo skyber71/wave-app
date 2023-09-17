79c082086b26bd3b2116ec0b8b5db47512325ff6
Kiran <mail.skyber71@gmail.com>
2023-09-17 15:44:14 +0530

    Commit

---
+++ app.js
@@ -0,0 +1,62 @@
+require("dotenv").config();
+const mongoose  = require("mongoose");
+const app = require("express")();
+const http = require("http").Server(app);
+mongoose.connect(process.env.MONGO_URL);
+const User  = require("./models/userModel")
+const userRoute = require("./routes/userRoute")
+app.use("/",userRoute);
+const Chat = require("./models/chatModel");
+const crypto = require("crypto");
+const algorithm = process.env.ALGORITHM;
+
+const io = require("socket.io")(http);
+var usp = io.of("/user-namespace");
+usp.on("connection",async (socket)=>{
+    console.log("User Connected");
+    var userId = socket.handshake.auth.token;
+    await User.findByIdAndUpdate({_id:userId},{$set:{ is_online:'1'}});
+    socket.broadcast.emit("getOnlineUser",{user_id:userId});
+
+    socket.on("disconnect",async ()=>{
+        console.log("Usr disconnected");
+        var userId = socket.handshake.auth.token;
+        await User.findByIdAndUpdate({_id:userId},{$set:{ is_online:'0'}});
+        socket.broadcast.emit("getOfflineUser",{user_id:userId});
+    });
+
+    socket.on("newChat",(data)=>{
+        socket.broadcast.emit("loadNewChat", {receiverId:data.receiverId, senderId:data.senderId, message:data.message, createdAt:data.createdAt});
+    });
+
+    socket.on("existsChat", async (data)=>{
+        var chats  = await Chat.find({ $or:[
+            {senderId: data.senderId,receiverId: data.receiverId},
+            {senderId: data.receiverId,receiverId: data.senderId},
+        ]});
+        const senderData = await User.findOne({_id: data.senderId});
+        const receiverData = await User.findOne({_id: data.receiverId});
+        const senderKey = senderData.key;
+        const receiverKey = receiverData.key;
+        let publicKey = process.env.KEY;
+        let key;
+        if(senderKey > receiverKey){
+            key = senderKey + receiverKey + publicKey;
+        }else{
+            key = receiverKey + senderKey + publicKey;
+        }
+        for(let i = 0;i<chats.length;i++){
+            const originalIV = Buffer.from(chats[i]["iv"], "base64");
+            const decipher = crypto.createDecipheriv(algorithm,key,originalIV);
+            let decryptedData = decipher.update(chats[i]["message"],"hex","utf-8");
+            decryptedData += decipher.final("utf-8");
+            chats[i]["message"] = decryptedData;
+        }
+        socket.emit("loadChat",{chats :chats});
+    })
+    socket.on("existsChat", async(data)=>{
+        var receiverUser = await User.findOne({_id: data.receiverId});
+        socket.emit("loadReceiver",{receiverUser: receiverUser});
+    })
+});
+http.listen(3000,()=>{console.log("Server Started on Port 3000");})
---
+++ models/userModel.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    password:{
+        type:String,
+        required: true
+    },
+    is_online:{
+        type: String,
+        default: '0'
+    },
+    image:{
+        type: String,
+        required: true
+    },
+    key:{
+        type: String,
+        required: true
+    }
+},
+{timestamps: true});
+
+module.exports = mongoose.model("User",userSchema);
